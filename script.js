51c196937ff1fd0d956a45ce18ef3e655236b06f
xypxzy <xypxzy@gmail.com>
2023-01-07 01:02:46 +0600

    Button problem

--- script.js
+++ script.js
@@ -1,7 +1,38 @@
 'use strict'
-const btn = document.querySelector('.switch')
+/*Selectors*/
+const btn = document.querySelector('.toggleSwitch')
+const learnMoreBtn = document.querySelector('.moreBtn')
+const prices = document.querySelectorAll('.price')
+const contentBlocks = document.querySelectorAll('.activeBlock')
+/*Changed Monthly/Yearly*/
+let i = 0;
+btn.addEventListener('click', (e) => {
+    let priceEl;
+    removePrices();
+    prices.forEach(price => {
 
-btn.addEventListener('click', (e)=>{
+        priceEl = Number(price.textContent);
+        price.textContent = `${Math.floor(priceEl * 11 / 2)}.99`
 
-    console.log(e)
+    })
 })
+
+
+contentBlocks.forEach(contentBlock => {
+    contentBlock.addEventListener('click', () => {
+        removeActiveClasses();
+        contentBlock.classList.add('active');
+    })
+})
+
+function removeActiveClasses() {
+    contentBlocks.forEach((content) => {
+        content.classList.remove('active')
+    })
+}
+
+function removePrices() {
+    prices[0].innerHTML = '19.99';
+    prices[1].innerHTML = '24.99';
+    prices[2].innerHTML = '39.99';
+}
