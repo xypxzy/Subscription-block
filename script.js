ca54d2995711f4579a868df69155f94f9e965e0b
xypxzy <xypxzy@gmail.com>
2023-01-07 01:17:22 +0600

    Finish active content and switch button

--- script.js
+++ script.js
@@ -1,23 +1,33 @@
 'use strict'
 /*Selectors*/
 const btn = document.querySelector('.toggleSwitch')
-const learnMoreBtn = document.querySelector('.moreBtn')
-const prices = document.querySelectorAll('.price')
 const contentBlocks = document.querySelectorAll('.activeBlock')
+const prices = document.querySelectorAll('.price')
+const learnMoreBtns = document.querySelectorAll('.moreBtn')
 /*Changed Monthly/Yearly*/
-let i = 0;
+let i = 0 , j = 4;
 btn.addEventListener('click', (e) => {
+    i += 1;
     let priceEl;
     removePrices();
     prices.forEach(price => {
-
         priceEl = Number(price.textContent);
-        price.textContent = `${Math.floor(priceEl * 11 / 2)}.99`
-
+        price.textContent = `${Math.floor(priceEl * 11 / 2)-0.01}`
     })
+
+    if (i == j) {
+        removePrices()
+        j += 4;
+    }
 })
 
+function removePrices() {
+    prices[0].innerHTML = '19.99';                          //Можно изменить на массивы
+    prices[1].innerHTML = '24.99';
+    prices[2].innerHTML = '39.99';
+}
 
+/*Active Content*/
 contentBlocks.forEach(contentBlock => {
     contentBlock.addEventListener('click', () => {
         removeActiveClasses();
@@ -31,8 +41,11 @@ function removeActiveClasses() {
     })
 }
 
-function removePrices() {
-    prices[0].innerHTML = '19.99';
-    prices[1].innerHTML = '24.99';
-    prices[2].innerHTML = '39.99';
-}
+/*Modal Window when clicking Button Learn More*/
+
+learnMoreBtns.forEach(learnMoreBtn => {
+    learnMoreBtn.addEventListener('click', ()=>{
+
+    })
+})
+
